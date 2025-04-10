134024057ac7c4ccc1141efc9ca97ebafe4b6132
Billnyethecoderguy <treytreyallen@gmail.com>
2025-04-10 19:25:28 -0400

    my second c app! (not finished :/)

---
+++ units.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+char main(void) {
+    while(1) {
+        int inp;
+        system("clear");
+        printf("==========\n   Menu   \n==========\n1. Temperatures\n2. Distance\n3. Money\n");
+        scanf("%i", &inp);
+        switch (inp) {
+            case 1:
+            //temps
+        }
+    }
+}
